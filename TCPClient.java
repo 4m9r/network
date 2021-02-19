fbd0f0655ec95009ce3a9a15d79c804e85c7d867
amir <amirali.safizadeh@gmail.com>
2021-02-19 15:17:13 +0100

    fix

--- TCPClient.java
+++ TCPClient.java
@@ -7,16 +7,19 @@ public class TCPClient {
     private static int BUFFER_SIZE = 1024;
     
     public static String askServer(String hostname, int port, String ToServer) throws IOException {
-        byte data[] = new byte[BUFFER_SIZE]; // to store data from the server
+        
         byte userInput [] = ToServer.getBytes(StandardCharsets.UTF_8);
+        int size = ToServer.length();
+        byte data[] = new byte[size];
 
         Socket clientSocket = new Socket(hostname,port);
         clientSocket.setSoTimeout(5000);
 
         clientSocket.getOutputStream().write(userInput); // send byte to the socket
-        int fromServerLength = clientSocket.getInputStream().read(data); // recive data from the socket 
 
-        String str = new String(data,0,fromServerLength,StandardCharsets.UTF_8);
+        
+        clientSocket.getInputStream().readNBytes(data,0,size);
+        String str = new String(data);
         clientSocket.close(); 
 
         return str;
@@ -33,10 +36,9 @@ public class TCPClient {
         // my attempt to deal with the last test
         while(true){
             int k = clientSocket.getInputStream().read(data);
-
             // if there is no data break, the second condition is for
             // the last task, it does not break without it
-            if(k < 0 || sb.length() > 15000) 
+            if(k < 0 || sb.length() > 14000) 
             break;
 
             for(int i = 0; i < k; i++)
@@ -46,7 +48,7 @@ public class TCPClient {
         }
 
         clientSocket.close();
-        
+    
         return sb.toString();
     }
 }
