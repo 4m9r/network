4decb1db6fe127a1f21b2d08e61e8a3da2a2d06a
amir <amirali.safizadeh@gmail.com>
2021-02-20 12:25:57 +0100

    vers 4 with one bug

--- TCPClient.java
+++ TCPClient.java
@@ -7,22 +7,39 @@ public class TCPClient {
     private static int BUFFER_SIZE = 1024;
     
     public static String askServer(String hostname, int port, String ToServer) throws IOException {
-        
+        byte data[] = new byte[BUFFER_SIZE]; // to store data from the server
         byte userInput [] = ToServer.getBytes(StandardCharsets.UTF_8);
-        int size = ToServer.length();
-        byte data[] = new byte[size];
-
-        Socket clientSocket = new Socket(hostname,port);
-        clientSocket.setSoTimeout(5000);
-
-        clientSocket.getOutputStream().write(userInput); // send byte to the socket
-
         
-        clientSocket.getInputStream().readNBytes(data,0,size);
-        String str = new String(data);
-        clientSocket.close(); 
+        
 
-        return str;
+        Socket clientSocket = new Socket(hostname, port);
+        clientSocket.setSoTimeout(5000);
+        
+
+        clientSocket.getOutputStream().write(userInput);
+
+        StringBuilder sb = new StringBuilder();
+        InputStream stream = clientSocket.getInputStream();
+        int length = stream.read(data,0,BUFFER_SIZE);
+        
+        if(length >= BUFFER_SIZE){
+        while(length != -1){
+            sb.append(new String(data, 0, length, StandardCharsets.UTF_8));
+
+            if(length < BUFFER_SIZE)
+            break;
+
+            length = stream.read(data, 0, BUFFER_SIZE); 
+            
+        }
+    }
+        else
+            sb.append(new String(data, 0, length, StandardCharsets.UTF_8));
+
+         
+        clientSocket.close();
+        
+        return sb.toString();
     }
 
     public static String askServer(String hostname, int port) throws IOException {
